79baea951d140cac6004fc2327d607c66eaf8a18
Cole Ternes <54868474+coleternes@users.noreply.github.com>
2022-08-30 14:46:21 -0700

    Rearranged ship class

--- Battleship/Ship.java
+++ Battleship/Ship.java
@@ -1,14 +1,21 @@
 public class Ship {
     // Member variables
     private int _length;
-    private Direction _orientation;
     private int[] _position;
+    private Direction _orientation;
 
     // Default constructor
+    public Ship() {
+        this.setLength(-1);
+        this.setPosition(new int[] {-1, -1});
+        this.setOrientation(Direction.EMPTY);
+    }
+
+    // Overloaded constructor
     public Ship(int length, Direction orientation, int[] position) {
-        this._length = length;
-        this._orientation = orientation;
-        this._position = position;
+        this.setLength(length);
+        this.setPosition(position);
+        this.setOrientation(orientation);
     }
 
     // Length mutator and accessor
@@ -19,19 +26,19 @@ public class Ship {
         this._length = length;
     }
 
-    // Orientation mutator and accessor
-    public Direction getOrientation() {
-        return this._orientation;
-    }
-    public void setOrientation(Direction orientation) {
-        this._orientation = orientation;
-    }
-
     // Position mutator and accessor
     public int[] getPosition() {
         return this._position;
     }
     public void setPosition(int[] position) {
         this._position = position;
+    }
+
+    // Orientation mutator and accessor
+    public Direction getOrientation() {
+        return this._orientation;
+    }
+    public void setOrientation(Direction orientation) {
+        this._orientation = orientation;
     }
 }
