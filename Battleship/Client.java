cb4e0c32acf65244796429d87c7912dfd598afc2
Cole Ternes <54868474+coleternes@users.noreply.github.com>
2022-08-31 13:28:13 -0700

    Laid groundwork for client-server interaction

--- Battleship/Client.java
+++ Battleship/Client.java
@@ -5,9 +5,95 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] argv) throws Exception {
-        // Instantiate the player object
+        // Instantiate the player object and setup the board
         Player p = new Player();
-
         p.setupBoard();
+
+        // Attempt socket connection
+        Socket clientSocket = null;
+        try {
+            clientSocket = new Socket("localhost", 6789);
+        } catch (Exception e) {
+            System.out.println("Failed to open socket connection");
+            System.exit(0);
+        }
+
+        // Instantiate server input / outut streams
+        BufferedReader serverReceiver =  new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        DataOutputStream serverSender = new DataOutputStream(clientSocket.getOutputStream());
+
+        // Check if it's the client's turn to shoot
+        // https://stackoverflow.com/questions/21365046/inputstream-read-read-wrong-boolean-value
+        boolean isMyTurn = serverReceiver.read() != 0;
+        System.out.println("isMyTurn = " + isMyTurn);
+
+        // Main game loop
+        while (p._running) {
+            // TODO:
+            // 1. Check if it's the client's turn to shoot
+            // 2. Player 1 will be first to shoot...
+            //    a. Player 1 inputs position
+            //    b. Player 1 sends position to server
+            //    c. Server sends position to Player 2
+            //    d. Player 2 updates shot data via _ship_board
+            //    e. Player 2 calculates _running & _victory
+            //    f. Player 2 sends shot data to server
+            //    g. Server sends shot data to Player 1
+            //    h. Player 1 updates _ship_board
+            //    i. Player 1 calculates _running & _victory
+            // 3. Print the boards
+            // 4. Inverse the isMyTurn value
+
+            // If it's my turn, shoot at a position and send it to the server
+            if (isMyTurn) {
+                // Determine the player's shot position
+                int[] shotPosition = p.fireShot();
+
+                // Send the position to the server
+                serverSender.writeInt(shotPosition[0]);
+                serverSender.writeInt(shotPosition[1]);
+
+                // When ready, receive the shot data
+                String shotData = serverReceiver.readLine();
+                System.out.println("Received shotdata = " + shotData);
+
+                // Update the player's _shot_board
+                p.updateShotBoard(shotPosition, shotData);
+            }
+            // Otherwise, wait to receive the eneimies shot
+            else {
+                System.out.println("\nWaiting for enemy's shot...");
+
+                // When ready, receive the enemy's shot
+                int row = serverReceiver.read();
+                int col = serverReceiver.read();
+                System.out.println("Received shotPosition: (" + row + ", " + col + ")");
+                int[] shotPosition = new int[] {row, col};
+
+                // Determine the shot data based on the position
+                String shotData = p.updateShipBoard(shotPosition);
+
+                // Send the shot data to the server
+                serverSender.writeBytes(shotData + '\n');
+            }
+
+            // Send the server the game status
+            serverSender.writeBoolean(p._running);
+
+            // Print both boards to the screen
+            p.printBoards();
+
+            // Inverse isMyTurn at the end of each round
+            isMyTurn = !isMyTurn;
+        }
+
+        // Determine the victor
+        if (p._victory)
+            System.out.println("You win!");
+        else
+            System.out.println("You lost!");
+
+        // Close the socket after game completion
+        clientSocket.close();
     }
 }
--- Battleship/Server.java
+++ Battleship/Server.java
@@ -4,6 +4,142 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+// Reference: https://www.geeksforgeeks.org/multithreaded-servers-in-java/
 public class Server {
+    public static void main(String[] argv) {
+        // Define the server socket and players
+        ServerSocket serverSocket = null;
+        ClientHandler player1 = null;
+        ClientHandler player2 = null;
+        boolean gameStarted = false;
 
+        try {
+            // Server is listening on port 6789
+            serverSocket = new ServerSocket(6789);
+            serverSocket.setReuseAddress(true);
+
+            // Infinite loop to always listen for client requests
+            while (true) {
+                // Instantiate the client socket
+                Socket clientSocket = serverSocket.accept();
+
+                // Store the players
+                if (player1 == null) {
+                    // Instantiate player1
+                    player1 = new ClientHandler(clientSocket, true);
+                    new Thread(player1).start();
+                }
+                else {
+                    if (player2 == null) {
+                        // Instantiate player2
+                        player2 = new ClientHandler(clientSocket, false);
+                        new Thread(player2).start();
+
+                        // Send the turn orders to initiate the game
+                        player1.sendTurnOrder();
+                        player2.sendTurnOrder();
+                        gameStarted = true;
+                    }
+                    else {
+                        // TODO: Send rejection message if game is running
+                    }
+                }
+
+                // If the game has started, process the incoming and outgoing data
+                if (gameStarted) {
+                    // Store the game state of player1 and player2
+                    boolean player1Running = true;
+                    boolean player2Running = true;
+
+                    // While loop to run until the game is over
+                    while (player1Running && player2Running) {
+                        // Check if it's player1's turn
+                        if (player1._is_my_turn) {
+                            // Grab player1's shot position
+                            int row = player1._client_receiver.read();
+                            int col = player1._client_receiver.read();
+
+                            // Send the shot position to player2
+                            player2._client_sender.writeInt(row);
+                            player2._client_sender.writeInt(col);
+
+                            // Grab the shot data from player2
+                            String shotData = player2._client_receiver.readLine();
+
+                            // Send the shot data to player1
+                            player1._client_sender.writeBytes(shotData + '\n');
+
+                            // Inverse the player's turn order
+                            player1._is_my_turn = false;
+                            player2._is_my_turn = true;
+                        }
+                        // Otherwise, it is player2's turn
+                        else {
+                            // Grab player2's shot position
+                            int row = player2._client_receiver.read();
+                            int col = player2._client_receiver.read();
+
+                            // Send the shot position to player1
+                            player1._client_sender.writeInt(row);
+                            player1._client_sender.writeInt(col);
+
+                            // Grab the shot data from player1
+                            String shotData = player1._client_receiver.readLine();
+
+                            // Send the shot data to player2
+                            player2._client_sender.writeBytes(shotData + '\n');
+
+                            // Inverse the player's turn order
+                            player1._is_my_turn = true;
+                            player2._is_my_turn = false;
+                        }
+
+                        // Update players game states
+                        player1Running = player1._client_receiver.read() != 0;
+                        player2Running = player2._client_receiver.read() != 0;
+                    }
+
+                    // At the end of the game, reset player1 and player2
+                    player1 = null;
+                    player2 = null;
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        // Member variables
+        private final Socket _client_socket;
+        public final BufferedReader _client_receiver;
+        public final DataOutputStream _client_sender;
+        public boolean _is_my_turn;
+
+        // Default constructor
+        public ClientHandler(Socket clientSocket, boolean isMyTurn) throws Exception {
+            this._client_socket = clientSocket;
+            this._client_receiver = new BufferedReader(new InputStreamReader(this._client_socket.getInputStream()));
+            this._client_sender = new DataOutputStream(this._client_socket.getOutputStream());
+            this._is_my_turn = isMyTurn;
+        }
+
+        // Method to determine turn order
+        public void sendTurnOrder() throws Exception {
+            this._client_sender.writeBoolean(this._is_my_turn);
+        }
+
+        public void run() { }
+    }
 }
